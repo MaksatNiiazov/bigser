355b8847785ac01ab04d4e7eae3f7d4d2042152e
maksat.niiazov <niiazov.maksat98@gmail.com>
2024-09-01 15:41:21 +0600

    init2

--- core/settings.py
+++ core/settings.py
@@ -135,7 +135,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-CSRF_TRUSTED_ORIGINS = '*'
+CSRF_TRUSTED_ORIGINS = ['*',]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
